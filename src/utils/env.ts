616f83ddcc6166e92a4451af9e47c7b049f28f21
drivanreis <dr.ivanreis@gmail.com>
2024-07-20 17:38:46 -0300

    Troca de endereço1

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -4,7 +4,7 @@ const getBackendURL = (): string => {
   if (import.meta.env.VITE_BACKEND_URL === undefined || import.meta.env.VITE_BACKEND_URL === '') {
     // return 'http://localhost:3001';
     // return 'http://189.71.214.150:3001';
-    return 'https://b07a-189-71-214-150.ngrok-free.app'
+    return 'https://9047-189-71-214-150.ngrok-free.app/'
   } else {
     return import.meta.env.VITE_BACKEND_URL;
   }
