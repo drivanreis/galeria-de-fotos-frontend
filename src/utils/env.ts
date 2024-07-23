05178396c892923ffc3856e6eae838c53f14523e
drivanreis <dr.ivanreis@gmail.com>
2024-07-23 20:17:32 -0300

    Trocando para aws

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -4,7 +4,8 @@ const getBackendURL = (): string => {
   if (import.meta.env.VITE_BACKEND_URL === undefined || import.meta.env.VITE_BACKEND_URL === '') {
     // return 'http://localhost:3001';
     // return 'http://189.71.214.150:3001';
-    return 'https://9047-189-71-214-150.ngrok-free.app/'
+    // return 'https://9047-189-71-214-150.ngrok-free.app/'
+    return 'https://orgiy71zxj.execute-api.us-east-1.amazonaws.com/';
   } else {
     return import.meta.env.VITE_BACKEND_URL;
   }
